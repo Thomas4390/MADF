b8769cd28f1a17c2ade5e86e43887abc6fb22850
Thomas Vaudescal <vaudescal.t@gmail.com>
2022-09-18 17:10:42 -0400

    [ADD] | creation of sp500 data by run_data.py

---
+++ run_data.py
@@ -0,0 +1,26 @@
+
+# Import the necessary modules
+import pandas as pd
+import yfinance as yf
+
+
+def get_sp_data(start='2008-01-01', end=None):
+    # Get the current SP components, and get a tickers list
+    sp_assets = pd.read_html(
+        'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
+    assets = sp_assets['Symbol'].str.replace('.', '-').tolist()
+    # Download historical data to a multi-index DataFrame
+    try:
+        data = yf.download(assets, start=start, end=end)
+        filename = 'sp_500_data.pkl'
+        data.to_pickle(filename)
+        print('Data saved at {}'.format(filename))
+    except ValueError:
+        print('Failed download, try again.')
+        data = None
+    return data
+
+
+if __name__ == '__main__':
+    sp_data = get_sp_data()
+
