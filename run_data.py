f18b874e59ead26917dafeb010c024b04242db18
Thomas Vaudescal <vaudescal.t@gmail.com>
2022-09-22 17:22:42 -0400

    [NEW] | adding new storage format HDF5

--- run_data.py
+++ run_data.py
@@ -12,8 +12,9 @@ def get_sp_data(start: str = "2008-01-01", end: str = None) -> pd.DataFrame:
     # Download historical data to a multi-index DataFrame
     try:
         data = yf.download(assets, start=start, end=end)
-        filename = "sp_500_data.pkl"
-        data.to_pickle(f"data/{filename}")
+        # HDF5 is a good format for storing large amounts of data
+        filename = "sp_500_data.hdf"
+        data.to_hdf(f"data/{filename}", key="df")
         print(f"Data saved at {filename}")
     except ValueError:
         print("Failed download, try again.")
