32d1634bfd7550a22fe0c11a6744fdcb974c41f7
Thomas Vaudescal <vaudescal.t@gmail.com>
2022-09-24 09:56:22 -0400

    [ADD] | commentaires pour la fonction find_n_pairs

--- functions/dataTransformation.py
+++ functions/dataTransformation.py
@@ -46,16 +46,20 @@ def find_n_pairs(df_corr: np.ndarray, n_max:int = 10) -> List[List]:
     :param df_corr: matrice de corrélation
     :param n_max: nombre de paires maximums à renvoyer
     :return: List[List] : La liste de toutes les n plus grandes paires.
+    On précise que même si elles ne sont pas triées par ordre décroissant
+    dans la liste retourné, les n plus grandes paires sont bien présentes.
     """
+
     # On convertit la matrice de corrélation au format ndarray
     df_corr_np = df_corr.to_numpy()
     # On remplit la diagonale de 1
     np.fill_diagonal(df_corr_np, 0)
     # Trouve les n plus grands indices dans l'array "flattened"
-    indices = (-df_corr_np).argpartition(n_max, axis=None)[:n_max]
+    indices = (np.abs(df_corr_np)).argpartition(n_max, axis=None)[:n_max]
     # Convertit les indices "flattened" en indices matriciels selon
     # le format de la matrice de corrélation
     x, y = np.unravel_index(indices, df_corr_np.shape)
     # Retrouve les paires associés avec les coordonnées x et y.
     pairs_list = [[df_corr.index[a], df_corr.columns[b]] for a, b in zip(x, y)]
     return pairs_list
+
