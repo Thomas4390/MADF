fa11b53b34fb6a91a61f27085e36b82bc137fb3e
SebLegros <sebastien.legros@hotmail.ca>
2022-09-24 16:05:14 -0400

    Réparation du code pour append les nouvelles variables et nouveau data frame qui indique quand on utilise quel paire de variables pour trader

--- RunningScrip/run_strategy.py
+++ RunningScrip/run_strategy.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 from Class.RollingWindow import MyRollingWindow
@@ -11,6 +12,7 @@ ROLLING_WINDOW = 60
 # Étape 1 : Importer les prix des titres à analyser
 # prixTitresSP = get_sp_data()
 prixTitres = importDataFromYahoo(['AAPL', 'MSFT', 'META']).dropna(how='any')
+# prixTitres = pd.read_hdf('../data/sp_500_data.hdf')
 
 # Étape 2 : Transformation des prix en rendement
 yieldTitresSP = transformPricesToYield(prixTitres)
@@ -19,8 +21,8 @@ yieldTitresSP = transformPricesToYield(prixTitres)
 # il faut une rolling window pour l'étape 4 à 8.
 
 rollingWindowData = MyRollingWindow(yieldTitresSP, window=ROLLING_WINDOW)
-newVar = pd.DataFrame(index=prixTitres.index)
-
+newVariableDataFrame = pd.DataFrame(index=prixTitres.index)
+newVariableToTradeDataFrame = pd.DataFrame(index=prixTitres.index)
 for windowData in rollingWindowData:
     # Étape 4 : Analyse de la dépendance (covariance) à chaque période.
     covMat = windowData.corr()
@@ -32,8 +34,22 @@ for windowData in rollingWindowData:
     # Étape 6 : Créer les nouvelles variables "X" à trader / analyser (différence ou ratio entre les paires des PRIX de titres sélectionnées)
     # (A - B) ou (A / B)
     pricesAfterRollingWindow = prixTitres.loc[prixTitres.index > windowData.index[-1], :]
-    newVar = newVar.join(create_variable_to_trade(pricesAfterRollingWindow, nPairs))
+    newVar = create_variable_to_trade(pricesAfterRollingWindow, nPairs)
 
+    #### METTRE DANS UNE NOUVELLE FONCTION POUR QUE "run_strategy.py" SOIT LISIBLE, SINON C'EST LAID À CHIER À TERRE
+    columnsToKeep = [True] * newVar.shape[1]
+    newColumns = newVar.columns
+    if not newVariableDataFrame.empty:
+        columnsToKeep = list(map(lambda x: x not in newVariableDataFrame.columns, newColumns))
+
+    for newcol in newColumns[columnsToKeep]:
+        newVariableToTradeDataFrame[newcol] = np.nan
+
+    newVariableDataFrame = newVariableDataFrame.join(newVar.loc[:, columnsToKeep])
+
+    newVarNextWindow = newVar.iloc[:ROLLING_WINDOW, :]
+    newVariableToTradeDataFrame.loc[newVarNextWindow.index, newColumns] = 1
+    ######## FIN DE NOUVELLE FONCTION
 
     # Étape 7 : Pour chacune des variables "X", on doit trouver des variables explicatives pour prédire le rendement futur.
     # Soit, les lags des rendements, indicateurs techniques, métriques macroéconomiques...
