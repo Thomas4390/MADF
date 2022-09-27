452149ef16ddc67813c570b083d4f9f25a3c3031
SebLegros <sebastien.legros@hotmail.ca>
2022-09-27 14:45:00 -0400

    Debut du modele pour random forest

--- RunningScrip/technicalIndicators.py
+++ RunningScrip/technicalIndicators.py
@@ -1,38 +1,42 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import talib
+# import talib
 
 from RunningScrip.newVariableForPairTrading import createModifiedVariableForPairTrading
-from functions.technicalIndicators import *
-# Importation des variables transformées
-newVariableDataFrame, newVariableToTradeDataFrame = createModifiedVariableForPairTrading(rollingWindow=250,
-                                                                                         numberOfPairsToTrade=2,
-                                                                                         method='alphaFactor')
-
-# Création du dataframe pour les indicateurs techniques
-indicatorsDataFrame = pd.DataFrame(index=newVariableDataFrame.index)
-
-# Ajout des indicateurs techniques dans le dataframe pour indicateur.
-indicatorsDataFrame = indicatorsDataFrame.join(addNewIndicator(aroon, 'aroon', newVariableDataFrame))
-indicatorsDataFrame = indicatorsDataFrame.join(MOM(newVariableDataFrame))
-# indicatorsDataFrame = indicatorsDataFrame.join(addNewIndicator(ADX, 'ADX', newVariableDataFrame))
-# indicatorsDataFrame = indicatorsDataFrame.join(addNewIndicator(MACD, 'MACD', newVariableDataFrame))
-# indicatorsDataFrame = indicatorsDataFrame.join(addNewIndicator(RSI, 'RSI', newVariableDataFrame))
-# indicatorsDataFrame = indicatorsDataFrame.join(addNewIndicator(SMI, 'SMI', newVariableDataFrame))
+from functions.technicalIndicatorsFunctions import *
 
 
-for column in newVariableDataFrame.columns:
-    plt.plot(np.log(1+newVariableDataFrame[column]), label=column)
-plt.legend()
+def getData(rollingWindow: int = 60, numberOfPairsToTrade: int = 2, method: str = 'alphaFactor'):
+    # Importation des variables transformées
+    newVariableDataFrame, newVariableToTradeDataFrame = createModifiedVariableForPairTrading(
+        rollingWindow=rollingWindow,
+        numberOfPairsToTrade=numberOfPairsToTrade,
+        method=method)
 
-# fig, ax = plt.subplots(2)
-# for column in newVariableDataFrame.columns:
-#     ax[0].plot(newVariableDataFrame[column], label=column)
-# ax[0].legend()
+    # Création du dataframe pour les indicateurs techniques
+    indicatorsDataFrame = pd.DataFrame(index=newVariableDataFrame.index)
 
-# for column in indicatorsDataFrame.columns:
-#     ax[1].plot(indicatorsDataFrame[column], label=column)
-# ax[1].plot(indicatorsDataFrame.iloc[:, 1], label=indicatorsDataFrame.columns[1])
-# ax[1].legend()
+    # Ajout des indicateurs techniques dans le dataframe pour indicateur.
+    indicatorsDataFrame = indicatorsDataFrame.join(addNewIndicator(aroon, 'aroon', newVariableDataFrame))
+    # indicatorsDataFrame = indicatorsDataFrame.join(MOM(newVariableDataFrame))
+    # indicatorsDataFrame = indicatorsDataFrame.join(addNewIndicator(ADX, 'ADX', newVariableDataFrame))
+    # indicatorsDataFrame = indicatorsDataFrame.join(addNewIndicator(MACD, 'MACD', newVariableDataFrame))
+    # indicatorsDataFrame = indicatorsDataFrame.join(addNewIndicator(RSI, 'RSI', newVariableDataFrame))
+    # indicatorsDataFrame = indicatorsDataFrame.join(addNewIndicator(SMI, 'SMI', newVariableDataFrame))
 
-plt.show()
+    # for column in newVariableDataFrame.columns:
+    #     plt.plot(np.log(1+newVariableDataFrame[column]), label=column)
+    # plt.legend()
+    #
+    # # fig, ax = plt.subplots(2)
+    # # for column in newVariableDataFrame.columns:
+    # #     ax[0].plot(newVariableDataFrame[column], label=column)
+    # # ax[0].legend()
+    #
+    # # for column in indicatorsDataFrame.columns:
+    # #     ax[1].plot(indicatorsDataFrame[column], label=column)
+    # # ax[1].plot(indicatorsDataFrame.iloc[:, 1], label=indicatorsDataFrame.columns[1])
+    # # ax[1].legend()
+    #
+    # plt.show()
+    return newVariableDataFrame, newVariableToTradeDataFrame, indicatorsDataFrame
